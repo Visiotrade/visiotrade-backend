bf5547bf8cf199f296c722a65a49fafe6bf2e464
Visiotrade <visiotradegmbh@gmail.com>
2026-03-29 22:35:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, { apiVersion: '2023-10-16' });
 const fetch = require('node-fetch');
 const { createTransport } = require('nodemailer');
 
